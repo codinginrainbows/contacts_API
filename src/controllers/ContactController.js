a1de4c3f0adef6c24326897a97674eee80a56eaf
gabriel garcia <gabrielgarcia.contactme@gmail.com>
2023-09-27 19:59:47 -0300

    feat: setup contacts controller

---
+++ src/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+    index(request, response) {
+        response.send('response sent from contact controller');
+    }
+    
+    show() {
+
+    }
+    
+    store() {
+
+    }
+    
+    update() {
+
+    }
+    
+    delete() {
+
+    }
+}
+
+module.exports = new ContactController();
